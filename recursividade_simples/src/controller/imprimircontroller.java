c58fbc5f93855aec0e3c046b584505b939da0741
Emily Furtado <95307737+emyrhf@users.noreply.github.com>
2026-03-16 17:17:12 -0300

    Add files via upload

---
+++ recursividade_simples/src/controller/imprimircontroller.java
@@ -0,0 +1,12 @@
+package controller;
+
+public class ImprimirController{
+    public ImprimirController(){
+        super();
+    }
+    public void imprimir(int numero){
+        if(numero>=10) return;
+        System.out.println("Número: " + numero);
+        imprimir(++numero);
+    }
+}
