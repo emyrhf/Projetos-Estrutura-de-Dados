9bf4f4e59ea39fac0b1009f8f1e2b85b16dc59fc
Emily Furtado <95307737+emyrhf@users.noreply.github.com>
2026-05-11 17:36:29 -0300

    Add files via upload

--- grafo_matriz/src/controller/GrafoMatrizController.java
+++ grafo_matriz/src/controller/GrafoMatrizController.java
@@ -8,15 +8,38 @@ public class GrafoMatrizController{
     }
 
     public String teste() throws Exception{
-        String[] routers = new String[]{"A", "B", "C", "D"};
+        String[] routers = new String[]{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O"};
         GrafoMatriz g = new GrafoMatriz(routers);
 
+        // g.link("A", "B");
+        // g.link("A", "D");
+        // g.link("B", "C");
+        // g.link("C", "D");
+
+        // g.remover("A", "D");
+
+        //Imputar grafo da imagem
         g.link("A", "B");
         g.link("A", "D");
+        g.link("A", "F");
         g.link("B", "C");
-        g.link("C", "D");
-
-        g.remover("A", "D");
+        g.link("C", "I");
+        g.link("C", "J");
+        g.link("D", "G");
+        g.link("D", "H");
+        g.link("D", "I");
+        g.link("E", "H");
+        g.link("E", "J");
+        g.link("F", "H");
+        g.link("F", "K");
+        g.link("G", "L");
+        g.link("H", "O");
+        g.link("I", "J");
+        g.link("J", "N");
+        g.link("K", "L");
+        g.link("L", "M");
+        g.link("M", "N");
+        g.link("N", "O");
 
         return g.toString();
     }
